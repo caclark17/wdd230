d4aea8cb47066033eb0d26cf2e7b4b2aff0e2582
caclark17 <cla20055@byui.edu>
2022-01-31 16:23:04 -0700

    added date

--- chamber/js/dates.js
+++ chamber/js/dates.js
@@ -1,1 +1,9 @@
 document.querySelector("#copyrightyear").textContent = new Date().getFullYear();
+
+const datefield = document.querySelector(".date");
+
+const now = new Date();
+const fulldate = new
+Intl.DateTimeFormat("en-us", {dateStyle: "full" }).format(now);
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
