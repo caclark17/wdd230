8e8c616ee26cc5421af2f64010abe84761811566
caclark17 <cla20055@byui.edu>
2022-02-15 21:34:31 -0700

    fix

--- chamber/js/windChill.js
+++ chamber/js/windChill.js
@@ -1,14 +1,14 @@
 let temp = parseFloat(document.querySelector('temp').value);
 let speed = parseFloat(document.querySelector('speed').value);
-let windchill = "";
+let windchill = '';
 
 if(temp <= 50 && speed > 3) {
     document.querySelector('wind').innerHTML = Chill.toFixed(2);
 } else {
-    windChill="N/A";
+    windchill="N/A";
 
-    function Chill(temp, speed) {
-        return 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+function Chill(temp, speed) {
+    return 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
     }
 }
 
