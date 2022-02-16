784a5410c0d75cc8c66933946ee239929f30de7e
caclark17 <cla20055@byui.edu>
2022-02-16 14:41:29 -0700

    changed day

--- chamber/js/banner.js
+++ chamber/js/banner.js
@@ -1,6 +1,6 @@
 let day= new Date().getDay();
 console.log(day);
-if (day == 1 || day == 3) {
+if (day == 1 || day == 2) {
     document.querySelector(".banner").classList.remove("hide");
 }
 else {
