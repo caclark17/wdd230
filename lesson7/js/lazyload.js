8f6eca15726b1cb290916a0f2420c3c2c9b3a1d1
caclark17 <cla20055@byui.edu>
2022-02-17 16:35:32 -0700

    lazy load

---
+++ lesson7/js/lazyload.js
@@ -0,0 +1,34 @@
+//get all images with data-src attribute
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+
+const imgOptions = {
+    rootMargin: '0 0 50px 0',
+    threshold: 1,
+};
+
+const loadImages = (image) => {
+    image.setAttribute('src',image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
+
+if ('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((items, imgObserver) => {
+        items.forEach((item) => {
+            if(item.isIntersecting) {
+                loadImages(item.target);
+                imgObserver.unobserve(item,target);
+            }
+        });
+    }, imgOptions);
+    
+    imagesToLoad.forEach((img) => {
+        imgObserver.observe(img);
+    });
+
+} else {
+    imagesToLoad.forEach((img) => {
+
+    });
+}
