66c6faf577a80a2fb77214c65d6d6d9e91fa14d0
caclark17 <cla20055@byui.edu>
2022-03-03 11:16:24 -0700

    prophets

--- lesson-9/js/prophets.js
+++ lesson-9/js/prophets.js
@@ -16,24 +16,31 @@ function displayProphets(prophet) {
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
     let portrait = document.createElement('img');
-    let dob = document.createElement('h3');
-    let pob = document.createElement('h3');
+    let birthDate = document.createElement('h3');
+    let birthPlace = document.createElement('h3');
+    let death = document.createElement('p')
+    let numofchildren = document.createElement('p')
 
     //change the textContent property of the h2 element to contain the prophet's full name
     h2.textContent = `${prophet.name} ${prophet.lastname}`;
-    dob.textContent = `Date of Birth: ${prophet.birthdate}`;
-    pob.textContent = `Place of Birth: ${prophet.birthplace}`;
+    birthDate.textContent = `Date of Birth: ${prophet.birthdate}`;
+    birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`;
+    death.textContent = `Date of Death: ${prophet.death}`
+    numofchildren.textContent = `Number of children: ${prophet.numofchildren}`;
+    
 
     //Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values.
     portrait.setAttribute('src', prophet.imageurl);
-    portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+    portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}, ${prophet.order} Latter-Day Prophet`);
     portrait.setAttribute('loading', 'lazy');
 
     // Add/apend the section(card) with the h2 element
     card.appendChild(h2);
     card.appendChild(portrait);
-    card.appendChild(dob);
-    card.appendChild(pob);
+    card.appendChild(birthDate);
+    card.appendChild(birthPlace);
+    card.appendChild(death);
+    card.appendChild(numofchildren);
 
     // Add/apend the existing HTML div with the cards class with the section(card)
     cards.appendChild(card);
