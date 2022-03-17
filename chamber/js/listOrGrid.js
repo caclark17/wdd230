da7a014c10fd6cfa028734362609e1bdb8e184e6
caclark17 <cla20055@byui.edu>
2022-03-17 13:22:50 -0600

    contact page

---
+++ chamber/js/listOrGrid.js
@@ -0,0 +1,6 @@
+const gridView = document.getElementsByClassName(".gridIcon");
+const listView = document.getElementsByClassName(".listIcon");
+
+function viewGrid() {
+    document.querySelector(".cards").classList.add
+}
