77c105268992b5b212b49edde2cf5dcea44e30ae
caclark17 <cla20055@byui.edu>
2022-03-30 23:40:55 -0600

    additions

--- templeProject/js/templeData.js
+++ templeProject/js/templeData.js
@@ -17,23 +17,21 @@ fetch(requestURL)
         let photo = document.createElement('img');
         let templeName = document.createElement('h2');
         let address = document.createElement('p');
-        let address2 = document.createElement('p');
         let phoneNum = document.createElement('p');
         let services = document.createElement('p');
         let history = document.createElement('p');
         let ordinanceSchedule = document.createElement('p');
         let sessionSchedule = document.createElement('p');
-        let closure = document.createElement('p');
+        let templeClosureSchedule = document.createElement('p');
 
         templeName.textContent = `${temples.location}`;
-        address.textContent = `${temples.address}`;
-        address2.textContent = `${temples.address2}`;
-        phoneNum.textContent = `${temples.phone}`;
-        services.textContent = `${temples.services}`;
-        history.textContent = `${temples.history}`;
-        ordinanceSchedule.textContent = `${temples.ordinance-schedule}`;
-        sessionSchedule.textContent = `${temples.session-schedule}`;
-        closure.textContent = `${temples.temple-closure-schedule}`;
+        address.textContent = `Address: ${temples.address} ${temples.address2}`;
+        phoneNum.textContent = `Phone Number: ${temples.phone}`;
+        services.textContent = `Services: ${temples.services}`;
+        history.textContent = `History: ${temples.history}`;
+        ordinanceSchedule.textContent = `Ordinance Schedule: ${temples.ordinanceSchedule}`;
+        sessionSchedule.textContent = `Session Schedule: ${temples.sessionSchedule}`;
+        templeClosureSchedule.textContent = `Closure Schedule: ${temples.templeClosureSchedule}`;
 
 
         photo.setAttribute('src', temples.photo);
@@ -42,14 +40,13 @@ fetch(requestURL)
 
         card.appendChild(photo);
         card.appendChild(templeName);
-        card.appendChild(address);
         card.appendChild(phoneNum);
-        card.appendChild(address2);
+        card.appendChild(address);
         card.appendChild(services);
         card.appendChild(history);
         card.appendChild(ordinanceSchedule);
         card.appendChild(sessionSchedule);
-        card.appendChild(closure);
+        card.appendChild(templeClosureSchedule);
 
         cards.appendChild(card);
     }
